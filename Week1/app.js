263da5ede320bba1f5a232d29b2bfc729488c7fa
Petrina Ada <petrina.ada@bryanuniversity.edu>
2022-09-15 18:50:18 -0700

    update week 1 assignments

---
+++ Week1/app.js
@@ -0,0 +1,11 @@
+const math = require('./math.js') ///exporting info from math.js
+///console.log(math)
+const result1 = math.addThis1(5,6)///using & importing info into result variable
+const result2 = math.subtractThis2(1,2)
+const result3 = math.divideThis3(4,9)
+const result4 = math.multiplyThis4(2,8)
+console.log(result1) ///review the result
+console.log(result2)
+console.log(result3)
+console.log(result4)
+///to view the output run node "filename"*/
---
+++ Week1/math.js
@@ -0,0 +1,5 @@
+function addThis1(num1, num2) {return num1 + num2}
+function subtractThis2(num1, num2) {return num1 - num2}
+function divideThis3(num1, num2) {return num1 / num2}
+function multiplyThis4(num1, num2) {return num1 * num2}
+module.exports = {addThis1, subtractThis2, divideThis3, multiplyThis4}
